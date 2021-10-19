d5c3ea31bed0cb694a77e92e7df45fd78c8b8d09
Bryan Bandela <bryan.bandela@gmail.com>
2021-10-19 12:34:21 +0200

    client & server on differenturl

--- server/server.js
+++ server/server.js
@@ -2,8 +2,13 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-app.use(express.static('public'));
+const cors = require('cors');
 app.use(express.json());
+app.use(
+  cors({
+    origin: 'http://localhost:5500', //we're currently running the server on port 5500. Also in the url input: replace http://127.0.0.1/ with localhost
+  })
+);
 
 //Pass the stripe key
 const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY);
@@ -32,8 +37,8 @@ app.post('/checkout-process-session', async (req, res) => {
           quantity: item.quantity,
         };
       }), //Array of items we're sending down to purchase and format them in a way that stripe expects them to
-      success_url: `${process.env.SERVER_URL}/success.html`, //where to send the user on success
-      cancel_url: `${process.env.SERVER_URL}/failure.html`, //where to send user on failure
+      success_url: `${process.env.CLIENT_URL}/success.html`, //where to send the user on success
+      cancel_url: `${process.env.CLIENT_URL}/failure.html`, //where to send user on failure
     });
     res.json({ url: session.url }); //the session has a url
   } catch (error) {
@@ -47,3 +52,7 @@ app.listen(3000, () => {
 
 //Note: When you change things in .env always restart the server
 //If your frontend and backend are not in the same folder then use a different method
+//In the second git push : I deal with different url
+//In the client's console log we'll face a CORS issue since the client (from a different url) tries to access a route in the server
+//To get around CORS: npm i cors in the server
+//Make sure you change the redirect to the appropriate page
